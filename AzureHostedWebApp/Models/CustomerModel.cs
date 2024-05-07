a8c6bb119638db6d928853a66a0b777790682b23
Kanishk Gujrati <kanishkgujrati@gmail.com>
2024-05-07 11:02:53 +0530

    Add project files.

---
+++ AzureHostedWebApp/Models/CustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AzureHostedWebApp.Models
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class CustomerDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set;}
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options) { 
+            
+        }
+    }
+}
---
+++ AzureHostedWebApp/Program.cs
@@ -0,0 +1,42 @@
+using AzureHostedWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureHostedWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connStr = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+
+            builder.Services.AddDbContext<CustomerDbContext>(options =>
+            {
+                options.UseSqlServer(connStr);
+            });
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
